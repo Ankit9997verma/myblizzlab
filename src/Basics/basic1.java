db523d2ba53fd811081c4f5cc40d7cffeeb8ea2b
Ankit9997verma <ankit9997verma@gmail.com>
2025-09-10 10:46:42 +0530

    Added Basics programs (basic1-basic10)

---
+++ src/Basics/basic1.java
@@ -0,0 +1,7 @@
+package Basics;
+
+public class basic1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
---
+++ src/Basics/basic10.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class basic10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double km = sc.nextDouble();
+
+        double miles = km * 0.621371;
+        System.out.println(km + " kilometers = " + miles + " miles");
+
+        sc.close();
+    }
+}
